9c76d59d32ddf0d0bc5dcc35dd099a7a30edd13e
Abad-ui <aaronabadv@gmail.com>
2024-12-13 01:00:43 +0800

    updated

--- app/src/main/java/com/example/activity1/MainActivity.kt
+++ app/src/main/java/com/example/activity1/MainActivity.kt
@@ -1,28 +1,29 @@
-package com.example.activity1
-import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import androidx.appcompat.app.AppCompatActivity
+    package com.example.activity1
+    import android.os.Bundle
+    import android.widget.ArrayAdapter
+    import android.widget.ListView
+    import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        val countryList = listOf(
-            "United States", "Canada", "Mexico", "Brazil", "Argentina",
-            "United Kingdom", "Germany", "France", "Italy", "Spain",
-            "China", "Japan", "India", "South Korea", "Australia",
-            "Russia", "South Africa", "Egypt", "Nigeria", "Kenya", "Saudi Arabia"
-        )
+            val countryList = listOf(
+                "UNITED STATES", "CANADA", "MEXICO", "BRAZIL", "ARGENTINA",
+                "UNITED KINGDOM", "GERMANY", "FRANCE", "ITALY", "SPAIN",
+                "CHINA", "JAPAN", "INDIA", "SOUTH KOREA", "AUSTRALIA",
+                "RUSSIA", "SOUTH AFRICA", "EGYPT", "NIGERIA", "KENYA"
+            )
 
-        val listView: ListView = findViewById(R.id.countryLV)
+            val listView: ListView = findViewById(R.id.countryListView)
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countryList)
+            val adapter = ArrayAdapter(this, R.layout.country_list, countryList)
 
-        // Set the adapter to the ListView
-        listView.adapter = adapter
+            // Set the adapter to the ListView
+            listView.adapter = adapter
+
+        }
     }
-}
 
